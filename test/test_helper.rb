db091ead60cb5ac498af879008f69e48654d76ba
Konstantin82off <k.udodov@internet.ru>
2026-04-21 20:20:06 +0300

    test: ensure user exists for hexlet-check compatibility

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,15 +7,8 @@ require "webmock/minitest"
 WebMock.disable_net_connect!(allow_localhost: true)
 
 class ActionDispatch::IntegrationTest
-  # Переопределяем метод get для всех тестов
-  def get(path, **kwargs)
-    kwargs[:headers] = { "Accept" => "text/html" }.merge(kwargs[:headers] || {})
-    super(path, **kwargs)
-  end
-
-  def post(path, **kwargs)
-    kwargs[:headers] = { "Accept" => "text/html" }.merge(kwargs[:headers] || {})
-    super(path, **kwargs)
+  setup do
+    User.find_or_create_by!(email: "one@test.io")
   end
 end
 
