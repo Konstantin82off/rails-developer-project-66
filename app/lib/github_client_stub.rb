5c1a117ffdc1bc1d0bb86c7795959952c12ee19b
Konstantin82off <k.udodov@internet.ru>
2026-04-30 18:39:32 +0300

    refactor: use fixture for GithubClientStub repos

--- app/lib/github_client_stub.rb
+++ app/lib/github_client_stub.rb
@@ -1,78 +1,18 @@
 # frozen_string_literal: true
 
 require "ostruct"
+require "json"
 
 class GithubClientStub
   def repos
-    [
-      OpenStruct.new(
-        id: 123,
-        name: "test-repo",
-        full_name: "testuser/test-repo",
-        language: "Ruby",
-        clone_url: "https://github.com/testuser/test-repo.git",
-        ssh_url: "git@github.com:testuser/test-repo.git"
-      ),
-      OpenStruct.new(
-        id: 345,
-        name: "hexlet-cv",
-        full_name: "Hexlet/hexlet-cv",
-        language: "Ruby",
-        clone_url: "https://github.com/Hexlet/hexlet-cv.git",
-        ssh_url: "git@github.com:Hexlet/hexlet-cv.git"
-      ),
-      OpenStruct.new(
-        id: 456,
-        name: "js-repo",
-        full_name: "testuser/js-repo",
-        language: "JavaScript",
-        clone_url: "https://github.com/testuser/js-repo.git",
-        ssh_url: "git@github.com:testuser/js-repo.git"
-      )
-    ]
+    fixture_path = Rails.root.join("test/fixtures/files/user_repositories.json")
+    content = File.read(fixture_path)
+    repositories = JSON.parse(content, symbolize_names: true)
+    repositories.map { |repo| OpenStruct.new(repo) }
   end
 
   def repo(id)
-    id_int = id.to_i
-
-    case id_int
-    when 123, 345
-      OpenStruct.new(
-        id: id_int,
-        name: id_int == 123 ? "test-repo" : "hexlet-cv",
-        full_name: id_int == 123 ? "testuser/test-repo" : "Hexlet/hexlet-cv",
-        language: "Ruby",
-        clone_url: "https://github.com/user/repo-#{id_int}.git",
-        ssh_url: "git@github.com:user/repo-#{id_int}.git"
-      )
-    when 456
-      OpenStruct.new(
-        id: 456,
-        name: "js-repo",
-        full_name: "testuser/js-repo",
-        language: "JavaScript",
-        clone_url: "https://github.com/testuser/js-repo.git",
-        ssh_url: "git@github.com:testuser/js-repo.git"
-      )
-    when 789
-      OpenStruct.new(
-        id: 789,
-        name: "py-repo",
-        full_name: "testuser/py-repo",
-        language: "Python",
-        clone_url: "https://github.com/testuser/py-repo.git",
-        ssh_url: "git@github.com:testuser/py-repo.git"
-      )
-    else
-      OpenStruct.new(
-        id: id_int,
-        name: "repo-#{id_int}",
-        full_name: "user/repo-#{id_int}",
-        language: "Ruby",
-        clone_url: "https://github.com/user/repo-#{id_int}.git",
-        ssh_url: "git@github.com:user/repo-#{id_int}.git"
-      )
-    end
+    repos.find { |r| r.id == id.to_i }
   end
 
   def create_hook(*)
