99258f89edef48a33e53419aa1d692b55b91dae8
Konstantin82off <k.udodov@internet.ru>
2026-04-30 18:46:37 +0300

    fix: github client stub returns fallback repo for any ID

--- app/lib/github_client_stub.rb
+++ app/lib/github_client_stub.rb
@@ -12,7 +12,19 @@ class GithubClientStub
   end
 
   def repo(id)
-    repos.find { |r| r.id == id.to_i }
+    id_int = id.to_i
+    existing = repos.find { |r| r.id == id_int }
+    return existing if existing
+
+    # Для любого несуществующего ID возвращаем репозиторий (Ruby по умолчанию)
+    OpenStruct.new(
+      id: id_int,
+      name: "repo-#{id_int}",
+      full_name: "user/repo-#{id_int}",
+      language: "Ruby",
+      clone_url: "https://github.com/user/repo-#{id_int}.git",
+      ssh_url: "git@github.com:user/repo-#{id_int}.git"
+    )
   end
 
   def create_hook(*)
