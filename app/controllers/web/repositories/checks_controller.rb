1fef2779d6aef5f500ba4163d0b9944126695f08
Konstantin82off <k.udodov@internet.ru>
2026-05-18 16:00:22 +0300

    fix: point 23 - replace before_action with explicit calls in ChecksController

--- app/controllers/web/repositories/checks_controller.rb
+++ app/controllers/web/repositories/checks_controller.rb
@@ -1,28 +1,20 @@
 # frozen_string_literal: true
 
 class Web::Repositories::ChecksController < Web::ApplicationController
-  before_action :authenticate_user!
-  before_action :set_repository
-  before_action :authorize_repository
-
   def show
+    authenticate_user!
+    @repository = Repository.find(params[:repository_id])
+    authorize @repository
     @check = @repository.checks.find(params[:id])
     render :show
   end
 
   def create
+    authenticate_user!
+    @repository = Repository.find(params[:repository_id])
+    authorize @repository
     @check = @repository.checks.create!(commit_id: 'pending', passed: false)
     RepositoryCheckJob.perform_later(@check.id)
     redirect_to repository_check_path(@repository, @check), notice: t('flash.check_created')
-  end
-
-  private
-
-  def set_repository
-    @repository = Repository.find(params[:repository_id])
-  end
-
-  def authorize_repository
-    authorize @repository
   end
 end
